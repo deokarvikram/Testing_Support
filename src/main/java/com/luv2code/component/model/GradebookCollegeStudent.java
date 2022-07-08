177ab5ce5cb1158a738185f4ba6f8d7080370d4d
Vikam <deokarvikram57@gmail.com>
2022-07-08 06:57:57 -0500

    temp

--- src/main/java/com/luv2code/component/model/GradebookCollegeStudent.java
+++ src/main/java/com/luv2code/component/model/GradebookCollegeStudent.java
@@ -1,4 +1,35 @@
 package com.luv2code.component.model;
 
-public class GradebookCollegeStudent {
+public class GradebookCollegeStudent extends CollegeStudent {
+    private int id;
+
+    private StudentGrades studentGrades;
+
+    public GradebookCollegeStudent(String firstname, String lastname, String emailAddress) {
+        super(firstname, lastname, emailAddress);
+    }
+
+    public GradebookCollegeStudent(int id, String firstname, String lastname, String emailAddress, StudentGrades studentGrades) {
+        super(firstname, lastname, emailAddress);
+        this.studentGrades = studentGrades;
+        this.id = id;
+    }
+
+    public StudentGrades getStudentGrades() {
+        return studentGrades;
+    }
+
+    public void setStudentGrades(StudentGrades studentGrades) {
+        this.studentGrades = studentGrades;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
 }
